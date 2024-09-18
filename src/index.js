9b8bea6ae2e71004d7d852f04ea70efd612db58e
faithmakhoali <faithmakhoali@gmail.com>
2024-09-18 17:09:25 +0200

    Amendded current weather selector to api structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.current.temperature.current);
+  let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
+  temperatureElement.innerHTML = temperature;
 }
 
 function search(event) {
@@ -14,7 +14,12 @@ function search(event) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios
+    .get(apiUrl)
+    .then(displayTemperature)
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+    });
 }
 
 function formatDate(date) {
@@ -62,7 +67,7 @@ function formatDate(date) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-let currentDateELement = document.querySelector("h1");
+let currentDateElement = document.querySelector("h1");
 let currentDate = new Date();
 
-currentDateELement.innerHTML = formatDate(currentDate);
+currentDateElement.innerHTML = formatDate(currentDate);
